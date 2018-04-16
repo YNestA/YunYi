f580ea6f1a711df187662d9344ff7d287076553f
feiyuhan <1191239053@qq.com>
2018-04-16 16:27:07 +0800

    图片

---
+++ src/component/mine/SettingShareSearch.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+import {View, Text, Image, StyleSheet, ScrollView} from 'react-native'
+import {screenUtils} from '../../tools/ScreenUtils'
+
+export class SettingShareSearch extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <View>
+                
+            </View>
+        );
+    }
+}
