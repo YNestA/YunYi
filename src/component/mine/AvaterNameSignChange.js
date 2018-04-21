cf2e49a8b022e3af84975a587b93a8cd74ca178f
feiyuhan <1191239053@qq.com>
2018-04-21 20:35:59 +0800

    这是mineview的redux

--- src/component/mine/AvaterNameSignChange.js
+++ src/component/mine/AvaterNameSignChange.js
@@ -1,21 +1,32 @@
 import React, {Component} from 'react'
 import {View, Text, Image, StyleSheet} from 'react-native'
+import {connect} from "react-redux"
+import NetworkError from '../../tools/NetworkError'
+import myFetch from '../../tools/MyFetch'
 import {screenUtils} from '../../tools/ScreenUtils'
 
-export class AvaterNameSignChange extends Component{
-    constructor(props){
+class AvaterNameSignChange extends Component {
+    constructor(props) {
         super(props);
+        console.log("this.props.userMessage", this.props.userMessage);
+        // console.log('this is props',this.props);
     }
-    render(){
-        return(
+
+    componentDidMount() {
+        this.props.initialAvasterSign();
+    }
+
+    render() {
+        console.log("render props userMessage", this.props.userMessage);
+        return (
             <View style={styles.avaterNameChange}>
                 <View style={styles.avater}>
                     <Image source={require('../../img/profilePic.jpg')} style={styles.cellFixed}/>
                 </View>
                 <View style={styles.profileNameAndSign}>
-                    <Text style={styles.userName}>我的名字</Text>
+                    <Text style={styles.userName}>{this.props.userMessage.userName}</Text>
                     <Text style={styles.Sign}
-                          numberOfLines={1}>你的名字是？你的名字是？dsfasdfasdfadsfadfasdfasdfasdfasdfasdfasdfa</Text>
+                          numberOfLines={1}>{this.props.userMessage.signDetail}</Text>
                 </View>
                 <View>
                     <Image source={require('../../img/profile-more.png')} style={styles.profileChange}/>
@@ -25,21 +36,71 @@ export class AvaterNameSignChange extends Component{
     }
 }
 
+let actions = {
+    // setNetworkError:function (value) {
+    //     return {
+    //         type:'NETWORK_ERROR',
+    //         payload: {
+    //             networkError: value,
+    //         }
+    //     }
+    // },
+    initialAvasterSign: function () {
+        return myFetch('http://www.baidu.com', {method: 'GET', timeout: 10000})
+            .then((response) => response.text())
+            .then((responseJson) => {
+                return {
+                    type: 'INITIAL_SIGN_NAME_AVATER',
+                    payload: {
+                        userMessage: userMessageExample
+                    }
+                }
+            }).catch((error) => {
+                console.log(error);
+            })
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        userMessage: state.userMessage
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        initialAvasterSign: () => {
+            dispatch(actions.initialAvasterSign())
+        }
+    }
+}
+
+export default AvaterNameSignChange = connect(mapStateToProps, mapDispatchToProps)(AvaterNameSignChange);
+
+const userMessageExample = {
+    avater: '../../img/profilePic.jpg',
+    userName: '安升强',
+    signDetail: '我是签名',
+    sex: '男',
+    iphoneNum: '15623425252',
+}
+
+
 const styles = StyleSheet.create({
-    avaterNameChange:{
-        flexDirection:'row',
-        justifyContent:'space-between',
-        alignItems:'center',
+    avaterNameChange: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
     },
-    avater:{
-        marginLeft:screenUtils.autoSize(8),
+    avater: {
+        marginLeft: screenUtils.autoSize(8),
     },
-    userName:{
-        fontSize:screenUtils.autoSize(16),
-        marginBottom:screenUtils.autoSize(3),
+    userName: {
+        fontSize: screenUtils.autoSize(16),
+        marginBottom: screenUtils.autoSize(3),
     },
     Sign: {
-        fontSize:screenUtils.autoSize(12),
+        fontSize: screenUtils.autoSize(12),
     },
     cellFixed: {
         height: screenUtils.autoSize(80),
@@ -47,7 +108,7 @@ const styles = StyleSheet.create({
         borderRadius: screenUtils.autoSize(80),
     },
     profileNameAndSign: {
-        marginLeft:screenUtils.autoSize(8),
+        marginLeft: screenUtils.autoSize(8),
         flex: 1,
     },
     profileChange: {
